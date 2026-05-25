d96603248cb49c9722a51cd285060485050a816d
washington renato alves <129201684+washington2953@users.noreply.github.com>
2026-05-25 19:56:35 -0300

    Remove Firebase SDK imports and configuration comments
    Removed Firebase SDK import statements and comments.

--- app.js
+++ app.js
@@ -7,13 +7,6 @@
 // Siga o guia em README.md para obter esses valores.
 // Depois de colar, remova esta linha de comentário.
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD2lRbR5TsZw5fUZi7e7sYwA2hs4SLORgs",
   authDomain: "guilda-poe2.firebaseapp.com",
